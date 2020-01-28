a6dbb7c7947c6d9e80419ffae51c6eef4a5fb389
mireiaGomariz <mireiapgomariz@gmail.com>
2020-01-28 16:48:52 +0100

    Chat-app exercise

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contact from './components/Contacts/contact.js'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Contact />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
